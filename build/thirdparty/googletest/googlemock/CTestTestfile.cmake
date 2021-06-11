f5a2a4f3a6ec315bfcc586d8f633cc87adfd588b
Preeti Yadav <preeti.ietlko@gmail.com>
2021-06-11 02:26:42 +0200

    Initial Commit

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/preeti/College/udacity/C++ Practise/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
+# Build directory: /home/preeti/College/udacity/C++ Practise/CppND-Route-Planning-Project/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
